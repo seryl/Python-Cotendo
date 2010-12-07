839c20e35f96497d8d235e7c4e7f42de5a83a28a
Josh Toft <joshtoft@gmail.com>
2010-12-07 12:15:48 -0800

    updated library, is now a proper egg

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(name='cotendo',
+      version=version,
+      description="Cotendo API",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='Cotendo Python API',
+      author='Josh Toft',
+      author_email='joshtoft@gmail.com',
+      url='https://github.com/seryl/Python-Cotendo',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          "lxml>=2.3beta1",
+          "BeautifulSoup>=3.2.0",
+          "suds>=0.4"
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
